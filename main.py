f5afa5031b78d248b032faed93b0c288730a756e
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-06-09 21:50:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ pin_Echo = DigitalPin.P15   #piny (+speedfactor)
 path = 1
 connected = 0
 switch = 0
-halt = True     #switch variables
+halt = True
+whileswitch = 0         #switch variables
 
 strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 pins.set_pull(pin_L, PinPullMode.PULL_NONE)
@@ -33,10 +34,9 @@ def on_forever():
         elif (sensor_L != path) and (sensor_R != path):
             motor_stop()
             motor_run(-50, 50)
-            basic.pause(300)
+            basic.pause(100)
             motor_run(50, -50)
-            basic.pause(600)
-            motor_stop()
+            basic.pause(100)
         elif (sensor_L == path) and (sensor_R != path):
             motor_run(50, 0)
         elif (sensor_L != path) and (sensor_R == path):
@@ -44,50 +44,3 @@ def on_forever():
     elif switch == 1:
         pass
 forever(on_forever)     #autonomní mód
-
-#NEKOMPLETNÍ ČÁST!!!ZÁKAZ VSTUPU
-
-def on_bluetooth_connected():
-    global connected, switch
-    basic.show_icon(IconNames.HEART)
-    connected = 1
-    while connected == 1:
-        uartData = bluetooth.uart_read_until(serial.delimiters(Delimiters.HASH))
-        console.log_value("data", uartData)
-        if uartData == "M":
-            switch == 1
-        if uartData == "L":
-            switch == 0
-        if uartData == "0":
-            motor_run(0, 0)
-        elif uartData == "A":
-            motor_run(50, 50)
-        elif uartData == "B":
-            motor_run(-50, -50)
-        elif uartData == "C":
-            motor_run(-50, 50)
-        elif uartData == "D":
-            motor_run(50, -50)
-        elif uartData == "G":
-            motor_run(-50, 50)
-            basic.pause(870)
-            motor_stop()
-        elif uartData == "H":
-            sensor_L = pins.digital_read_pin(pin_L)
-            sensor_R = pins.digital_read_pin(pin_R)
-            motor_run(-50, 50)
-            basic.pause(400)
-            motor_stop()
-            while (sensor_L != path) and (sensor_R != path):
-                motor_run(-50, 50)
-                if (sensor_L == path) or (sensor_R == path):
-                    motor_stop()
-                    break
-bluetooth.on_bluetooth_connected(on_bluetooth_connected)                    #bluetooth mód
-
-def on_bluetooth_disconnected():
-    global connected
-    basic.show_icon(IconNames.SAD)
-    connected = 0
-bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
-bluetooth.uart_write_number(0)
