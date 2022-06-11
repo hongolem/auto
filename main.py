313141ff27875f62bbcc8315d470ef4d48428c3a
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-06-11 17:54:35 +0200

    Update README.md, main.py

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ speedFactor = 80
 pin_L = DigitalPin.P13
 pin_R = DigitalPin.P2
 pin_Trig = DigitalPin.P8
-pin_Echo = DigitalPin.P15   #piny (+speedfactor)
+pin_Echo = DigitalPin.P15   #piny
 
 path = 1
 connected = 0
-switch = 0        #switch variables
+switch = 0
+counting = 0
+speedFactor = 80    #switchable variables
 
 strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 pins.set_pull(pin_L, PinPullMode.PULL_NONE)
 pins.set_pull(pin_R, PinPullMode.PULL_NONE)
 bluetooth.start_uart_service()
-basic.show_string("S")                                          #minor less important code
+basic.show_string("S")                                          #less important code
 
-def motor_run(right = 0, left = 0, speed_factor = 80):
+def motor_run(right = 0, left = 0, speedFactor = 80):
     PCAmotor.motor_run(PCAmotor.Motors.M1, Math.map(Math.constrain(left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
     PCAmotor.motor_run(PCAmotor.Motors.M4, Math.map(Math.constrain(right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
     
@@ -23,6 +25,7 @@ def motor_stop():
     PCAmotor.motor_stop(PCAmotor.Motors.M4)             #run/stop motors
     
 def on_forever():
+    global counting
     if switch == 0:
         sensor_L = pins.digital_read_pin(pin_L)
         sensor_R = pins.digital_read_pin(pin_R)
@@ -31,10 +34,18 @@ def on_forever():
             motor_run(50, 50)
         elif (sensor_L != path) and (sensor_R != path):
             motor_stop()
-            motor_run(-50, 50)
-            basic.pause(100)
-            motor_run(50, -50)
-            basic.pause(100)
+            if counting >= 10:
+                motor_run(-50, 50, 80)
+                basic.pause(870)
+                motor_stop()
+                counting = 0
+            elif counting < 10:
+                counting = counting + 1
+                motor_stop()
+                motor_run(-50, 50)
+                basic.pause(100)
+                motor_run(50, -50)
+                basic.pause(100)
         elif (sensor_L == path) and (sensor_R != path):
             motor_run(50, 0)
         elif (sensor_L != path) and (sensor_R == path):
@@ -46,41 +57,46 @@ forever(on_forever)     #autonomní mód
 #NEKOMPLETNÍ ČÁST!!!ZÁKAZ VSTUPU
 
 def on_bluetooth_connected():
-    global connected, switch
+    global connected, switch, path, speedFactor
     basic.show_icon(IconNames.HEART)
     connected = 1
     while connected == 1:
         uartData = bluetooth.uart_read_until(serial.delimiters(Delimiters.HASH))
         console.log_value("data", uartData)
-        if uartData == "M":
-            switch == 1
-        if uartData == "L":
-            switch == 0
+        print(uartData)
         if uartData == "0":
-            motor_run(0, 0)
+            pass
         elif uartData == "A":
             motor_run(50, 50)
         elif uartData == "B":
             motor_run(-50, -50)
         elif uartData == "C":
-            motor_run(-50, 50)
+            motor_run(50, 0)
         elif uartData == "D":
-            motor_run(50, -50)
+            motor_run(0, 50)
+        elif uartData == "E":
+            speedFactor = speedFactor - 5
+        elif uartData == "F":
+            speedFactor = speedFactor + 5
         elif uartData == "G":
-            motor_run(-50, 50)
+            if path == 0:
+                path = 1
+            elif path == 1:
+                path = 0
+        elif uartData == "H":
+            motor_run (50, -50, 80)
             basic.pause(870)
             motor_stop()
-        elif uartData == "H":
-            sensor_L = pins.digital_read_pin(pin_L)
-            sensor_R = pins.digital_read_pin(pin_R)
-            motor_run(-50, 50)
-            basic.pause(400)
-            motor_stop()
-            while (sensor_L != path) and (sensor_R != path):
-                motor_run(-50, 50)
-                if (sensor_L == path) or (sensor_R == path):
-                    motor_stop()
-                    break
+        elif uartData == "I":
+            pass
+        elif uartData == "J":
+            pass
+        elif uartData == "K":
+            pass
+        elif uartData == "L":
+            pass
+        elif uartData == "M":
+            pass
 bluetooth.on_bluetooth_connected(on_bluetooth_connected)                    #bluetooth mód
 
 def on_bluetooth_disconnected():
